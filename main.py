720d78fe7785a5c19cb76f519be408830616421f
tiago-at-slalom <tiago.nunes@slalom.com>
2020-02-07 14:52:39 +0000

    fix for teams hook url

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from powerSensor import PowerSensor
 from databaseAdaptor import DatabaseAdaptor
 from sensorHistoryDatabase import SensorHistoryDatabase
 
-url = "https://outlook.office.com/webhook/b07c187d-c2ad-49bd-a17d-39eba101d006@9ca75128-a244-4596-877b-f24828e476e2/IncomingWebhook/5a54226a4fc54de8a4b1fc626bb8c894/b5cacced-92a8-4dd0-b61c-328924fc32fb"
+url = "https://outlook.office.com/webhook/664a924c-d306-4a91-a953-c4b555c5484e@9ca75128-a244-4596-877b-f24828e476e2/IncomingWebhook/cfb87b236f254cbfb770f070a5e1b892/b5cacced-92a8-4dd0-b61c-328924fc32fb"
 
 sensorAdaptor = SensorAdaptor()
 powerSensor = PowerSensor(sensorAdaptor)
--- msTeamsConnector.py
+++ msTeamsConnector.py
@@ -2,7 +2,7 @@ import pymsteams
 
 class MsTeamsConnector:
     # TODO: move this to the config file
-    url = "https://outlook.office.com/webhook/664a924c-d306-4a91-a953-c4b555c5484e@9ca75128-a244-4596-877b-f24828e476e2/IncomingWebhook/cfb87b236f254cbfb770f070a5e1b892/b5cacced-92a8-4dd0-b61c-328924fc32fb"
+    url = ""
 
     def __init__(self, url):
         self.url = url
