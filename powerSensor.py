142f292215a2acf3c4c2505a2576fcbc1c91aa9c
tiago-at-slalom <tiago.nunes@slalom.com>
2020-02-07 15:06:39 +0000

    setting INPUT PIN number in the right place

--- powerSensor.py
+++ powerSensor.py
@@ -4,13 +4,13 @@ import RPi.GPIO as GPIO
 class PowerSensor:
     # sensorAdaptor = None # Do we need this?
     sensor = None
+    INPUT_PIN = 14           # Sets our input pin, in this example I'm connecting our button to pin 4. Pin 0 is the SDA pin so I avoid using it for sensors/buttons
 
     def __init__(self, sa):
         # self.sensorAdaptor = sa
         try:
             # self.sensor = SmoothedInputDevice(14)#, threshold = 1)
             GPIO.setmode(GPIO.BCM)           # Set's GPIO pins to BCM GPIO numbering
-            INPUT_PIN = 14           # Sets our input pin, in this example I'm connecting our button to pin 4. Pin 0 is the SDA pin so I avoid using it for sensors/buttons
             GPIO.setup(INPUT_PIN, GPIO.IN)           # Set our input pin to be an input
         except:
             print("Exception: Problem connecting to Input")
