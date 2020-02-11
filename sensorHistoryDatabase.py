828fb29819c719b511562f4cca3d23aa63ccc7a9
tiago-at-slalom <tiago.nunes@slalom.com>
2020-02-11 11:39:08 +0000

    changing logs location to /var/log folder and increasing lookup interval

--- sensorHistoryDatabase.py
+++ sensorHistoryDatabase.py
@@ -3,6 +3,7 @@ from time import time, ctime
 
 class SensorHistoryDatabase:
     lookupInterval = 0
+    logFilePath = '/var/log/dishwasherLogs.log'
 
     def __init__(self, lookupInterval):
         self.lookupInterval = lookupInterval
@@ -10,13 +11,13 @@ class SensorHistoryDatabase:
     def hasBeenRunningRecently(self):
         nowMinusABit = time() - self.lookupInterval
 
-        with open('logs.log', 'r') as f:
+        with open(self.logFilePath, 'r') as f:
             logLines = f.readlines()
             logLines.reverse()
 
             firstLine = logLines[0].strip().split("|")
             lineTime = int(firstLine[0])
-            
+
             if firstLine[2] == "MACHINE_NOT_RUNNING":
                 for line in logLines[1:]:
                     lineBits = line.strip().split("|")
@@ -45,7 +46,7 @@ class SensorHistoryDatabase:
     def logEvent(self, message):
         now = time()
         nowText = ctime(now)
-        with open('logs.log', 'a') as f:
+        with open(self.logFilePath, 'a') as f:
             f.write('%d|%s|%s\n' % (now, nowText, message))
         
         print(message)
